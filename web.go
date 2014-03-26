a6d4ccd5477f3267f0de759e8e0da859c6a25e2f
Luis Iván <me@luisivan.net>
2014-03-26 01:11:47 +0100

    Port fixed

--- web.go
+++ web.go
@@ -23,6 +23,6 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":80", nil)
 	log.Println("Server started")
 }
